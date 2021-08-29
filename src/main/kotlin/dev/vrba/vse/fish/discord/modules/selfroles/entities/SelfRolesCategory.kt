689f53f57975214ba35e70b6e80c3674856a0af9
Jiří Vrba <jiri@vrba.dev>
2021-08-29 09:31:27 +0200

    Self role category entity + repository

---
+++ src/main/kotlin/dev/vrba/vse/fish/discord/modules/selfroles/entities/SelfRolesCategory.kt
@@ -0,0 +1,30 @@
+package dev.vrba.vse.fish.discord.modules.selfroles.entities
+
+import javax.persistence.*
+
+@Entity
+@Table(
+    name = "self_role_categories",
+    uniqueConstraints = [
+        UniqueConstraint(
+            name = "UC_message",
+            columnNames = [
+                "guild",
+                "channel",
+                "message"
+            ]
+        )
+    ]
+)
+class SelfRolesCategory(
+    @Id
+    @GeneratedValue
+    val id: Long,
+
+    val name: String,
+
+    // IDs leading to a message with the roles menu
+    val guild: Long,
+    val channel: Long,
+    val message: Long,
+)
---
+++ src/main/kotlin/dev/vrba/vse/fish/discord/modules/selfroles/repositories/SelfRoleCategoriesRepository.kt
@@ -0,0 +1,8 @@
+package dev.vrba.vse.fish.discord.modules.selfroles.repositories
+
+import dev.vrba.vse.fish.discord.modules.selfroles.entities.SelfRolesCategory
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface SelfRoleCategoriesRepository : CrudRepository<SelfRolesCategory, Long>
