8afdce5aaedd546b651b996d7deb79a9a076759e
Jiří Vrba <jiri@vrba.dev>
2021-08-29 09:11:27 +0200

    Work in progress module for self roles

---
+++ src/main/kotlin/dev/vrba/vse/fish/discord/modules/selfroles/SelfRolesReactionsListener.kt
@@ -0,0 +1,7 @@
+package dev.vrba.vse.fish.discord.modules.selfroles
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.springframework.stereotype.Component
+
+@Component
+class SelfRolesReactionsListener : ListenerAdapter()
